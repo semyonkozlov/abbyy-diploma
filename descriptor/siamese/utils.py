11808dee03bb74bd8795e387f3a405135656f83d
Semyon <semkozloff@gmail.com>
2018-04-05 01:15:15 +0300

    Some fixes

--- descriptor/siamese/utils.py
+++ descriptor/siamese/utils.py
@@ -17,40 +17,22 @@ def cut_patch_from_bitmap(bitmap, image_index):
 
     return bitmap[image_row * 64 : (image_row + 1) * 64, image_col * 64 : (image_col + 1) * 64]
 
-def get_train_generator(dataset, num_pairs, batch_size=128):    
+def get_generator(dataset, num_pairs, batch_size=128):    
     i = 0
     input_a, input_b, targets = [], [], []
     
     match_file = pd.read_csv(f'{dataset}/m50_{num_pairs}_{num_pairs}_0.txt', delimiter=' ', 
         names=['patchID1', '3DpointID1', 'unused1', 'patchID2','3DpointID2', 'unused2', 'unused3'])
-    
+        
     while True:
         if i % batch_size == 0 and i != 0:
-            yield [np.array(x).reshape(batch_size, 64, 64, 1) for x in [input_a, input_b]], targets
+            assert len(input_a) == len(input_b) == batch_size
+            yield [np.asarray(input_a), np.asarray(input_b)], targets
             input_a, input_b, targets = [], [], []
             
         line = match_file.iloc[i % num_pairs]
-        input_a.append(read_patch(dataset, line['patchID1']))
-        input_b.append(read_patch(dataset, line['patchID2']))
-        targets.append(1 if line['3DpointID1'] == line['3DpointID2'] else 0)
+        input_a.append(read_patch(dataset, line['patchID1']).reshape(64, 64, 1))
+        input_b.append(read_patch(dataset, line['patchID2']).reshape(64, 64, 1))
+        targets.append(1 if line['3DpointID1'] == line['3DpointID2'] else -1)
         
         i += 1
-        
-def get_test_generator(dataset, num_pairs, batch_size=128):
-    i = 0
-    input_a, input_b = [], []
-    
-    match_file = pd.read_csv(f'{dataset}/m50_{num_pairs}_{num_pairs}_0.txt', delimiter=' ', 
-        names=['patchID1', '3DpointID1', 'unused1', 'patchID2','3DpointID2', 'unused2', 'unused3'])
-    
-    while True:
-        if i % batch_size == 0 and i != 0:
-            yield [np.array(x).reshape(batch_size, 64, 64, 1) for x in [input_a, input_b]], targets
-            input_a, input_b = [], []
-            
-        line = match_file.iloc[i % num_pairs]
-        input_a.append(read_patch(dataset, line['patchID1']))
-        input_b.append(read_patch(dataset, line['patchID2']))
-        
-        i += 1
-    
