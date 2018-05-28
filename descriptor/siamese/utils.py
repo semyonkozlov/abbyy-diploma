98b82964fc9781019f4f7517df4fd461d147493f
semyonkozlov <semkozloff@gmail.com>
2018-05-28 09:42:20 +0000

    Add dataset augmentation

--- descriptor/siamese/utils.py
+++ descriptor/siamese/utils.py
@@ -2,8 +2,13 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import keras.backend as K
-from functools import lru_cache
+
+import os
+import cv2
 from imgaug import augmenters as iaa
+from tqdm import tqdm
+
+from functools import lru_cache
 
 @lru_cache(maxsize=None)
 def read_bitmap(dataset_name, bitmap_index):
@@ -74,3 +79,48 @@ def show_images(images, n_images_in_row=4, titles=None):
         plt.imshow(image.squeeze(), cmap='gray')
         splt.set_title(title)
     plt.show()
+    
+@lru_cache(maxsize=8)
+def read_image(img_file):
+    return cv2.cvtColor(cv2.imread(img_file), cv2.COLOR_RGB2GRAY)
+
+def read_keypoint_patch(img_file, x, y, kp_size):
+    img = read_image(img_file)
+    x = int(x)
+    y = int(y)
+    r = max([int(np.ceil(kp_size)) // 2, 32])
+    return cv2.resize(img[y - r : y + r, x - r : x + r], (64, 64))
+
+def create_aug_dataset(src_dir, dest_dir, aug):
+    if not os.path.exists(dest_dir):
+        os.makedirs(dest_dir)
+        
+    sift = cv2.xfeatures2d.SIFT_create(100)
+    aug_det = aug.to_deterministic()
+    
+    for img_file in tqdm(os.listdir(src_dir)):
+        img = read_image(img_file)
+        aug_img = aug.augment_images([img])[0]
+
+        sift_kps = sift.detect(img)
+        kps = ia.KeypointsOnImage([ia.Keypoint(x=int(kp.pt[0]), y=int(kp.pt[1])) for kp in sift_kps], shape=img.shape)
+        aug_kps = aug.augment_keypoints([kps])[0]
+
+        kp_file = pd.DataFrame(columns=['src_img_file', 'src_x', 'src_y', 'aug_img_file', 'aug_x', 'aug_y', 'kp_size'])
+
+        src_xy = kps.get_coords_array()
+        aug_xy = aug_kps.get_coords_array()
+        kp_file['src_x'] = src_xy[:, 0]
+        kp_file['src_y'] = src_xy[:, 1]
+        kp_file['aug_x'] = aug_xy[:, 0]
+        kp_file['aug_y'] = aug_xy[:, 1]
+        kp_file['kp_size'] = [int(np.ceil(kp.size)) for kp in sift_kps]
+
+        aug_img_file = os.path.join(dest_dir, os.path.basename(img_file))
+        kp_file['aug_img_file'] = aug_img_file
+        kp_file['src_img_file'] = img_file
+
+        cv2.imwrite(aug_img_file, aug_img)
+        pre, ext = os.path.splitext(aug_img_file)
+        kp_file.to_csv(f'{pre}.csv')
+    
